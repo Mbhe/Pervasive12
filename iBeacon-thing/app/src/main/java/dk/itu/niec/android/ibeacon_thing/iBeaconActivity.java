fba351f4b9f1b6371d42d01cfcd963a1140acf62
niels <niec@itu.dk>
2017-02-23 14:06:32 +0100

    Added the app-directory from my iBeacon-thing project. Try to see if it can also compile on your machine.

---
+++ iBeacon-thing/app/src/main/java/dk/itu/niec/android/ibeacon_thing/iBeaconActivity.java
@@ -0,0 +1,88 @@
+package dk.itu.niec.android.ibeacon_thing;
+
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.Context;
+import android.content.Intent;
+import android.os.Handler;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+
+public class iBeaconActivity extends AppCompatActivity {
+
+    public final static int REQUEST_ENABLE_BT = 1;
+    // Stops scanning after 10 seconds.
+    private static final long SCAN_PERIOD = 10000;
+
+
+    private BluetoothAdapter mBluetoothAdapter;
+    //private LeDeviceListAdapter mLeDeviceListAdapter;
+    private boolean mScanning;
+    private Handler mHandler;
+
+    private TextView devices;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_i_beacon);
+
+        devices = (TextView) findViewById(R.id.list_of_devices);
+
+        // Initializes Bluetooth adapter.
+        final BluetoothManager bluetoothManager =
+                (BluetoothManager) getSystemService(Context.BLUETOOTH_SERVICE);
+        mBluetoothAdapter = bluetoothManager.getAdapter();
+
+        // Ensures Bluetooth is available on the device and it is enabled. If not,
+        // displays a dialog requesting user permission to enable Bluetooth.
+        if (mBluetoothAdapter == null || !mBluetoothAdapter.isEnabled()) {
+            Intent enableBtIntent = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBtIntent, REQUEST_ENABLE_BT);
+        }
+
+        scanLeDevice(true);
+    }
+
+
+    private void scanLeDevice(final boolean enable) {
+        if (enable) {
+            // Stops scanning after a pre-defined scan period.
+            /*mHandler.postDelayed(new Runnable() {
+                @Override
+                public void run() {
+                    mScanning = false;
+                    mBluetoothAdapter.stopLeScan(mLeScanCallback);
+                }
+            }, SCAN_PERIOD);*/
+
+            mScanning = true;
+            devices.append("We are starting!");
+            mBluetoothAdapter.startLeScan(mLeScanCallback);
+        } else {
+            mScanning = false;
+            mBluetoothAdapter.stopLeScan(mLeScanCallback);
+        }
+
+    }
+
+
+    // Device scan callback.
+    private BluetoothAdapter.LeScanCallback mLeScanCallback =
+            new BluetoothAdapter.LeScanCallback() {
+                @Override
+                public void onLeScan(final BluetoothDevice device, int rssi,
+                                     byte[] scanRecord) {
+                    runOnUiThread(new Runnable() {
+                        @Override
+                        public void run() {
+                            devices.append(device.toString());
+                        }
+                    });
+                }
+            };
+
+
+}
